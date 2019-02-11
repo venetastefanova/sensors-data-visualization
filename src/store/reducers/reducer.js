288edd5517a8fed762f4bb4bde29063d2e3d93be
Veneta Stefanova <venetastefanovaa@gmail.com>
2019-02-11 19:33:04 +0200

    reducer clean up

--- src/store/reducers/reducer.js
+++ src/store/reducers/reducer.js
@@ -1,10 +1,11 @@
 import * as actionTypes from "../actions/actionTypes";
 import { getSensorDataToday } from "./utilityFunctions/todayStatistics";
+import { getLatestEntry } from "./utilityFunctions/latestEntry";
 
 const initialState = {
   latestEntry: [],
   sensorOneToday: [],
-  sensorTwoToday:[],
+  sensorTwoToday: [],
   sensorThreeToday: [],
   sensorFourToday: []
 };
@@ -13,35 +14,18 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     //get latest entry
     case actionTypes.FETCH_LATEST_ENTRY:
-      var dataEntry = action.entry.pop();
-      console.log(action.entry);
-      // strip data so it is compatible for the graph values
-      //create object for each sensor and puts them in an array
-      for (var entry in dataEntry) {
-        if (dataEntry.hasOwnProperty(entry)) {
-          let latestFeed = {};
-          var currentEntry = Object.keys(dataEntry).map(entry => {
-            return (latestFeed = {
-              x: entry,
-              y: dataEntry[entry]
-            });
-          });
-          var dataEntryArray = currentEntry.slice(2, 6);
-        }
-      }
       return {
         ...state,
-        latestEntry: dataEntryArray
+        latestEntry: getLatestEntry(action.entry)
       };
     //get all entries
     case actionTypes.FETCH_TODAYS_ENTRIES:
       return {
         ...state,
-        sensorOneToday:getSensorDataToday(action.entries, "sensor1"),
+        sensorOneToday: getSensorDataToday(action.entries, "sensor1"),
         sensorTwoToday: getSensorDataToday(action.entries, "sensor2"),
         sensorThreeToday: getSensorDataToday(action.entries, "sensor3"),
-        sensorFourToday: getSensorDataToday(action.entries, "sensor4"),
-
+        sensorFourToday: getSensorDataToday(action.entries, "sensor4")
       };
 
     default:
---
+++ src/store/reducers/utilityFunctions/latestEntry.js
@@ -0,0 +1,18 @@
+export const getLatestEntry = (sensorsEntry) => {
+    var dataEntry = sensorsEntry.pop();
+    // strip data so it is compatible for the graph values
+    //create object for each sensor and puts them in an array
+    for (var entry in dataEntry) {
+      if (dataEntry.hasOwnProperty(entry)) {
+        let latestFeed = {};
+        var currentEntry = Object.keys(dataEntry).map(entry => {
+          return (latestFeed = {
+            x: entry,
+            y: dataEntry[entry]
+          });
+        });
+        var dataEntryArray = currentEntry.slice(2, 6);
+      }
+      return dataEntryArray;
+    }
+}
